a36b46cde8ba31407963121a6a593e638424c314
Laura Ohrndorf <laura.ohrndorf@gmail.com>
2022-12-16 17:01:12 +0100

    Add tailwind

--- postcss.config.js
+++ postcss.config.js
@@ -1,18 +1,17 @@
 const purgecss = require('@fullhuman/postcss-purgecss')({
-    content: ['./src/site/**/*.njk'],
+  content: ['./src/site/**/*.njk'],
 });
 
 module.exports = {
-    plugins: [
-        require('postcss-advanced-variables'),
-        require('postcss-import'),
-        require('postcss-custom-properties'),
-        require('postcss-nested'),
-        require('postcss-media-minmax'),
-        require('postcss-custom-media'),
-        require('autoprefixer'),
-        ...(process.env.NODE_ENV === 'production'
-            ? [purgecss, require('cssnano')]
-            : []),
-    ],
+  plugins: [
+    require('tailwindcss'),
+    require('postcss-advanced-variables'),
+    require('postcss-import'),
+    require('postcss-custom-properties'),
+    require('postcss-nested'),
+    require('postcss-media-minmax'),
+    require('postcss-custom-media'),
+    require('autoprefixer'),
+    ...(process.env.NODE_ENV === 'production' ? [purgecss, require('cssnano')] : []),
+  ],
 };
