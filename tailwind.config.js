6d21121f116e546ffe2b2fcee70b8f254afbe1b6
Laura Ohrndorf <laura.ohrndorf@gmail.com>
2022-12-16 23:15:00 +0100

    Move more to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,15 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['src/**/*.njk'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        serif: ['Source Serif Pro', ...defaultTheme.fontFamily.serif],
+        sans: ['Inter VF', ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [],
 };
