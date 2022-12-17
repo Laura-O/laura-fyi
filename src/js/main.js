ab9bc16ea1fca955d147302feb81fb89816703c8
Laura Ohrndorf <laura.ohrndorf@gmail.com>
2022-12-17 23:19:42 +0100

    Make navigation responsive

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,8 @@
-console.log('Hello, world!');
+// grab everything we need
+const btn = document.querySelector('button.mobile-menu-button');
+const menu = document.querySelector('.mobile-menu');
+
+// add event listeners
+btn.addEventListener('click', () => {
+  menu.classList.toggle('hidden');
+});
