fff415886e676c605724afaa198ef6db4b207586
Laura Ohrndorf <laura.ohrndorf@gmail.com>
2023-02-19 13:31:53 +0100

    Move shortcode to individual files

--- .eleventy.js
+++ .eleventy.js
@@ -3,87 +3,11 @@ const markdownIt = require('markdown-it');
 const markdownItAttrs = require('markdown-it-attrs');
 const excerpt = require('./src/plugins/excerpt');
 const shortcodes = require('./src/utils/shortcodes');
-const Image = require('@11ty/eleventy-img');
-const path = require('path');
-const outdent = require('outdent');
 const { DateTime } = require('luxon');
 
-const stringifyAttributes = (attributeMap) => {
-  return Object.entries(attributeMap)
-    .map(([attribute, value]) => {
-      if (typeof value === 'undefined') return '';
-      return `${attribute}="${value}"`;
-    })
-    .join(' ');
-};
-
-const imageShortcode = async (
-  src,
-  alt,
-  className = undefined,
-  widths = [400, 800, 1280],
-  formats = ['webp', 'jpeg'],
-  sizes = '100vw'
-) => {
-  const imageMetadata = await Image(src, {
-    widths: [...widths, null],
-    formats: [...formats, null],
-    outputDir: '_site/assets/images',
-    urlPath: '/assets/images',
-  });
-
-  const sourceHtmlString = Object.values(imageMetadata)
-    // Map each format to the source HTML markup
-    .map((images) => {
-      // The first entry is representative of all the others
-      // since they each have the same shape
-      const { sourceType } = images[0];
-
-      // Use our util from earlier to make our lives easier
-      const sourceAttributes = stringifyAttributes({
-        type: sourceType,
-        // srcset needs to be a comma-separated attribute
-        srcset: images.map((image) => image.srcset).join(', '),
-        sizes,
-      });
-
-      // Return one <source> per format
-      return `<source ${sourceAttributes}>`;
-    })
-    .join('\n');
-
-  const getLargestImage = (format) => {
-    const images = imageMetadata[format];
-    return images[images.length - 1];
-  };
-
-  const largestUnoptimizedImg = getLargestImage(formats[0]);
-  const imgAttributes = stringifyAttributes({
-    src: largestUnoptimizedImg.url,
-    width: largestUnoptimizedImg.width,
-    height: largestUnoptimizedImg.height,
-    alt,
-    loading: 'lazy',
-    decoding: 'async',
-  });
-  const imgHtmlString = `<img ${imgAttributes}>`;
-
-  const pictureAttributes = stringifyAttributes({
-    class: className,
-  });
-  const picture = `<picture ${pictureAttributes}>
-    ${sourceHtmlString}
-    ${imgHtmlString}
-    <p class="mt-0 text-xs">${alt}</p>
-  </picture>`;
-
-  return outdent`${picture}`;
-};
+const { imageShortcode } = require('./config/shortcodes');
 
 module.exports = (config) => {
-  // Compress and combine JS files
-  config.addFilter('jsmin', require('./src/utils/minify-js.js'));
-
   config.addPlugin(eleventyGoogleFonts);
 
   let options = {
@@ -111,8 +35,42 @@ module.exports = (config) => {
   });
   config.addPassthroughCopy({ 'node_modules/@fortawesome/fontawesome-free/webfonts': 'webfonts' });
 
+  config.setFrontMatterParsingOptions({
+    excerpt: true,
+    excerpt_separator: '<!-- excerpt -->',
+  });
+
+  // Collections
   config.addCollection('blog', (collection) => {
     return [...collection.getFilteredByGlob('./src/site/posts/*.md')].reverse();
+  });
+
+  config.addCollection('postsByYear', (collection) => {
+    const posts = collection.getFilteredByTag('posts').reverse();
+    const years = posts.map((post) => post.date.getFullYear());
+    const uniqueYears = [...new Set(years)];
+
+    const postsByYear = uniqueYears.reduce((prev, year) => {
+      const filteredPosts = posts.filter((post) => post.date.getFullYear() === year);
+
+      return [...prev, [year, filteredPosts]];
+    }, []);
+
+    return postsByYear;
+  });
+
+  // Filter
+  // Compress and combine JS files
+  config.addFilter('jsmin', require('./src/utils/minify-js.js'));
+  config.addFilter('filterTagList', filterTagList);
+  config.addFilter('getReadingTime', (text) => {
+    const wordsPerMinute = 200;
+    const numberOfWords = text.split(/\s/g).length;
+    return Math.ceil(numberOfWords / wordsPerMinute);
+  });
+  // Return the smallest number argument
+  config.addFilter('min', (...numbers) => {
+    return Math.min.apply(null, numbers);
   });
 
   // https://html.spec.whatwg.org/multipage/common-microsyntaxes.html#valid-date-string
@@ -136,50 +94,16 @@ module.exports = (config) => {
     return array.slice(0, n);
   });
 
-  // Return the smallest number argument
-  config.addFilter('min', (...numbers) => {
-    return Math.min.apply(null, numbers);
-  });
-
   function filterTagList(tags) {
     return (tags || []).filter((tag) => ['all', 'nav', 'post', 'posts'].indexOf(tag) === -1);
   }
 
-  config.addFilter('filterTagList', filterTagList);
-
-  config.addCollection('postsByYear', (collection) => {
-    const posts = collection.getFilteredByTag('posts').reverse();
-    const years = posts.map((post) => post.date.getFullYear());
-    const uniqueYears = [...new Set(years)];
-
-    const postsByYear = uniqueYears.reduce((prev, year) => {
-      const filteredPosts = posts.filter((post) => post.date.getFullYear() === year);
-
-      return [...prev, [year, filteredPosts]];
-    }, []);
-
-    return postsByYear;
-  });
-
-  config.setFrontMatterParsingOptions({
-    excerpt: true,
-    excerpt_separator: '<!-- excerpt -->',
-  });
-
+  // Shortcodes
+  config.addPairedShortcode('quote', shortcodes.quote);
   config.addShortcode('excerpt', (article) => {
     return excerpt(article);
   });
-
   config.addAsyncShortcode('myImage', imageShortcode);
-
-  config.addFilter('getReadingTime', (text) => {
-    const wordsPerMinute = 200;
-    const numberOfWords = text.split(/\s/g).length;
-    return Math.ceil(numberOfWords / wordsPerMinute);
-  });
-
-  // Shortcodes
-  config.addPairedShortcode('quote', shortcodes.quote);
 
   return {
     templateFormats: ['njk', 'md', '11ty.js'],
