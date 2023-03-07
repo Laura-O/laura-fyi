1bfa2aa29b63cd65092a7e0acec627c75f76c412
Laura Ohrndorf <laura.ohrndorf@gmail.com>
2023-03-07 23:17:46 +0100

    Color fixes (#12)
    * Minor css fixes
    
    * Minor css fixes
    
    * More fixes, mobile menu and about page

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,13 @@
 // grab everything we need
 const btn = document.querySelector('button.mobile-menu-button');
 const menu = document.querySelector('.mobile-menu');
+const menuLinks = document.querySelector('.mobile-nav-item');
 
 // add event listeners
 btn.addEventListener('click', () => {
+  menu.classList.toggle('hidden');
+});
+
+menuLinks.addEventListener('click', () => {
   menu.classList.toggle('hidden');
 });
